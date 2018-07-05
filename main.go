145418950814403bf13bad8cc46ba91a019d3924
zzayne <thezhangwen@outlook.com>
2018-07-06 01:54:19 +0800

    init

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/zzayne/zayne-crawler/fetcher"
 )
 
 func main() {
-	content, err := fetcher.Fetch("http://www.baidu.com")
+	contents, err := fetcher.Fetch("https://www.douban.com/location/shenzhen/events/week-all")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(content))
+
+	parser(contents)
+}
+
+func parser(contents []byte) {
+	activityRe := `<span itemprop="summary">([^>]+)</span>`
+	regx := regexp.MustCompile(activityRe)
+	matches := regx.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("sumarry title:%s\n", string(m[1]))
+	}
 }
