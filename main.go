daf5e5ea68667bfeb2bd76350690b0cac1b0af66
zzayne <thezaynezhang@gmail.com>
2022-04-09 23:32:11 +0800

    update go mod

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	var reqList []engine.Request
 
 	//链家的租房无明显入口，每页内容也无较好方法提取的下一页url，所以先手动提供120个分页入口来爬取内容
-	for i := 1; i < 120; i++ {
+	for i := 1; i < 1; i++ {
 		req := engine.Request{
 			URL:       "https://sz.lianjia.com/zufang/pg" + strconv.Itoa(i),
 			ParseFunc: parser.RentListParser,
